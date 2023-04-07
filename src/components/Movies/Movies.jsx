51aff402739028997d4cdca26cd3a4ce08aafec9
GUchechi <godswilluchendu7@gmail.com>
2023-04-07 14:24:36 +0100

    Movie and refactoring

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,134 +1,48 @@
-import image4 from "../../assets/image 4.png";
-import image5 from "../../assets/image 5.png";
-import image6 from "../../assets/image 6.png";
-import image8 from "../../assets/image 8.png";
-import image9 from "../../assets/image 9.png";
-import image10 from "../../assets/image 10.png";
+import { useState, useEffect } from "react";
 import "./Movies.css";
 
 const Movies = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await fetch("https://swapi.dev/api/films/");
+      const data = await response.json();
+      setMovies(data.results);
+    };
+    fetchMovies();
+  }, []);
+
   return (
     <main className="movies">
-      <div className="movies__container">
-        <img src={image4} alt="" />
-        <div className="movie__info">
-          <h3>Movie Title</h3>
-          <div className="movie__results">
-            <p className="movie__genre">
-              <span className="red__subGroup">Genre: </span>
-              Action-Adventure, Science Fiction
-            </p>
-            <p className="movie__date">
-              <span className="red__subGroup">Release Date: </span>
-              13th December 2016
-            </p>
-            <p className="movie__director">
-              <span className="red__subGroup">Director: </span>
-              Gareth Edwards
-            </p>
+      {movies.map((movie) => (
+        <div className="movies__container" key={movie.episode_id}>
+          <div className="movie__image--card">
+            <img
+              src={`https://starwars-visualguide.com/assets/img/films/${movie.episode_id}.jpg`}
+              alt={movie.title}
+              className='movie__image'
+            />
+          </div>
+          <div className="movie__info">
+            <h3>{movie.title}</h3>
+            <div className="movie__results">
+              <p className="movie__genre">
+                <span className="red__subGroup">Genre: </span>
+                {`Action-Adventure, Science Fiction`}
+              </p>
+              <p className="movie__date">
+                <span className="red__subGroup">Release Date: </span>
+                {movie.release_date}
+              </p>
+              <p className="movie__director">
+                <span className="red__subGroup">Director: </span>
+                {movie.director}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="movies__container">
-        <img src={image5} alt="" />
-        <div className="movie__info">
-          <h3>Movie Title</h3>
-          <div className="movie__results">
-            <p className="movie__genre">
-              <span className="red__subGroup">Genre: </span>
-              Action-Adventure, Science Fiction
-            </p>
-            <p className="movie__date">
-              <span className="red__subGroup">Release Date: </span>
-              13th December 2016
-            </p>
-            <p className="movie__director">
-              <span className="red__subGroup">Director: </span>
-              Gareth Edwards
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="movies__container">
-        <img src={image6} alt="" />
-        <div className="movie__info">
-          <h3>Movie Title</h3>
-          <div className="movie__results">
-            <p className="movie__genre">
-              <span className="red__subGroup">Genre: </span>
-              Action-Adventure, Science Fiction
-            </p>
-            <p className="movie__date">
-              <span className="red__subGroup">Release Date: </span>
-              13th December 2016
-            </p>
-            <p className="movie__director">
-              <span className="red__subGroup">Director: </span>
-              Gareth Edwards
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="movies__container">
-        <img src={image8} alt="" />
-        <div className="movie__info">
-          <h3>Movie Title</h3>
-          <div className="movie__results">
-            <p className="movie__genre">
-              <span className="red__subGroup">Genre: </span>
-              Action-Adventure, Science Fiction
-            </p>
-            <p className="movie__date">
-              <span className="red__subGroup">Release Date: </span>
-              13th December 2016
-            </p>
-            <p className="movie__director">
-              <span className="red__subGroup">Director: </span>
-              Gareth Edwards
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="movies__container">
-        <img src={image9} alt="" />
-        <div className="movie__info">
-          <h3>Movie Title</h3>
-          <div className="movie__results">
-            <p className="movie__genre">
-              <span className="red__subGroup">Genre: </span>
-              Action-Adventure, Science Fiction
-            </p>
-            <p className="movie__date">
-              <span className="red__subGroup">Release Date: </span>
-              13th December 2016
-            </p>
-            <p className="movie__director">
-              <span className="red__subGroup">Director: </span>
-              Gareth Edwards
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="movies__container">
-        <img src={image10} alt="" />
-        <div className="movie__info">
-          <h3>Movie Title</h3>
-          <div className="movie__results">
-            <p className="movie__genre">
-              <span className="red__subGroup">Genre: </span>
-              Action-Adventure, Science Fiction
-            </p>
-            <p className="movie__date">
-              <span className="red__subGroup">Release Date: </span>
-              13th December 2016
-            </p>
-            <p className="movie__director">
-              <span className="red__subGroup">Director: </span>
-              Gareth Edwards
-            </p>
-          </div>
-        </div>
-      </div>
+      ))}
     </main>
   );
 };
